a53c56a2ab238c533efd682d195037dc410e3a40
Twisted11121 <137068558+Twisted11121@users.noreply.github.com>
2025-03-31 11:35:47 +0000

    fixing login & registration

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ con_login = sqlite3.connect("login.db")
 cur_login = con_login.cursor()
 cur_login.execute("CREATE TABLE IF NOT EXISTS login(username, password)")
 con_login.commit()
-
+con_login.close()
 app.secret_key = "WellOffToVisitYourMother!" 
 
 
@@ -28,6 +28,8 @@ def index():
 
 @app.route("/login",  methods=['POST'])
 def login():
+    con_login = sqlite3.connect("login.db")
+    cur_login = con_login.cursor()
     
 
     try:
@@ -38,19 +40,29 @@ def login():
         info2 = info.fetchall()
         if password in info2:
             con_login.commit()
+            con_login.close()
             return render_template('/')
         
     except:
+        con_login.close()
         return render_template('register-acc.html')
 
+@app.route('/registerPage')
+def showRegi():
+    return render_template('register-acc.html') 
 
-@app.route("/register")
+@app.route("/register", methods=['POST'])
 def register():
+
+    con_login = sqlite3.connect("login.db")
+    cur_login = con_login.cursor()
+
     username = request.form["username"]
     password = request.form["password"]
     
-    reg_user = cur_login.execute(f"INSER INTO login(username, password) VALUES ({username}, {password})")
-
+    reg_user = cur_login.execute(f"INSERT INTO login(username, password) VALUES ({username}, {password})")
+    con_login.commit()
+    con_login.close()
     return jsonify({'success': True})
 
 
